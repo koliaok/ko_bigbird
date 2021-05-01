48377f97ad923741919da0c050c5a3d0fbbabb1d
bigtree <koliaok>
2021-05-01 16:01:17 +0900

    change server script

--- docker_up_pretraining_test.sh
+++ docker_up_pretraining_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /var/lib/docker/volumes/vol/_data/ko_bigbird/
+cd /home/bigtree/hyungrak/bigbird/
 
 /usr/local/bin/docker-compose run -d backend /app/ko_bigbird/bigbird/pretrain/run_pretraining_test.sh
